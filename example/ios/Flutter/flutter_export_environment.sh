dbc0d587c041ed8e9419dfa64c22f17b4353f518
Szalók Dániel <daniel.szalok@chameleon.sh>
2025-09-21 20:30:25 +0200

    bump

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Repositories\sqfEntity_stewioie\example"
+export "FLUTTER_ROOT=/Users/daniel/Work/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/daniel/Work/Repositories/sqfEntity_stewioie/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.3.0"
 export "FLUTTER_BUILD_NUMBER=1.3.0"
